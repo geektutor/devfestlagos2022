9ba25ed8a3b3b13ae8ac56d1a379e795d6dc5cd1
Iyinoluwa <dairoiyin@gmail.com>
2022-10-26 16:51:34 +0100

    added view directions popup

--- lib/views/maps_page/maps_page.dart
+++ lib/views/maps_page/maps_page.dart
@@ -1,6 +1,7 @@
 import 'package:devfest/core/router/navigator.dart';
 import 'package:devfest/core/state/providers.dart';
 import 'package:devfest/utils/constants.dart';
+import 'package:devfest/views/maps_page/maps_search_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -20,6 +21,8 @@ class _MapsPageState extends ConsumerState<MapsPage> {
   final LatLng destination = const LatLng(6.4230432, 3.4450862);
 
   List<LatLng> polylineCoordinates = [];
+
+  var currentLocation;
 
   void getPolyPoint() async {
     PolylinePoints polylinePoints = PolylinePoints();
@@ -77,10 +80,15 @@ class _MapsPageState extends ConsumerState<MapsPage> {
             Positioned(
               top: 70,
               left: 24,
-              right: 20,
+              right: 24,
               child: GestureDetector(
                 onTap: () {
-                  AppNavigator.pushNamed(Routes.mapsSearch);
+                  // AppNavigator.pushNamed(Routes.mapsSearch);
+                  var pickedLocation = Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MapSearchPage()));
+                  setState(() {
+                    currentLocation = pickedLocation;
+                  });
                 },
                 child: Container(
                   //  width: double.infinity,
@@ -108,6 +116,79 @@ class _MapsPageState extends ConsumerState<MapsPage> {
                 ),
               ),
             ),
+
+            //Directions Component
+
+            currentLocation == null
+                ? SizedBox()
+                : Positioned(
+                    top: 144,
+                    left: 24,
+                    right: 24,
+                    child: Container(
+                      //  width: double.infinity,
+                      padding: EdgeInsets.all(16),
+
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                              Image.asset(
+                                'assets/images/png/Rectangle 39.png',
+                                width: 80,
+                                height: 80,
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    'Your Destination ETA is',
+                                    style: TextStyle(
+                                        color: AppColors.grey0, fontSize: 16),
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  Text(
+                                    '2 hr 34 min away',
+                                    style: TextStyle(
+                                        color: Color(0xff4285F4), fontSize: 28),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 24,
+                          ),
+                          //View Direction Button
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: Text(
+                                'View Directions',
+                                style: TextStyle(
+                                    color: AppColors.white, fontSize: 18),
+                              ),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Color(0xff4285F4),
+                                  padding: EdgeInsets.symmetric(vertical: 24),
+                                  shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.all(
+                                          Radius.circular(88)))),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
           ]),
         ),
       ),
--- lib/views/maps_page/maps_search_page.dart
+++ lib/views/maps_page/maps_search_page.dart
@@ -24,69 +24,141 @@ class _MapSearchPageState extends ConsumerState<MapSearchPage> {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.only(top: 22, left: 32),
+          padding: EdgeInsets.only(top: 22, left: 32, right: 24),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Icon(
-                      Icons.arrow_back,
-                      size: 20,
+                  Expanded(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 16,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          child: Icon(
+                            Icons.arrow_back,
+                            size: 20,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    width: 14,
+                  // SizedBox(
+                  //   width: 14,
+                  // ),
+                  Spacer(),
+                  Expanded(
+                    flex: 2,
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 16,
+                        ),
+                        buildDot(
+                            size: 24,
+                            color: Color(0xffF0F6FE),
+                            child: Center(
+                                child: buildDot(
+                                    size: 8, color: Color(0xff4285F4)))),
+                        SizedBox(
+                          height: 9,
+                        ),
+                        buildDot(size: 4, color: AppColors.grey16),
+                        SizedBox(
+                          height: 9,
+                        ),
+                        buildDot(size: 4, color: AppColors.grey16),
+                        SizedBox(
+                          height: 9,
+                        ),
+                        buildDot(size: 4, color: AppColors.grey16),
+                        SizedBox(
+                          height: 12.25,
+                        ),
+                        Icon(
+                          Icons.location_on_outlined,
+                          color: Color(0xffEA4335),
+                        )
+                      ],
+                    ),
                   ),
-                  Column(
-                    children: [
-                      buildDot(
-                          size: 24,
-                          color: Color(0xffF0F6FE),
-                          child: Center(
-                              child:
-                                  buildDot(size: 8, color: Color(0xff4285F4)))),
-                      SizedBox(
-                        height: 9,
+                  // SizedBox(
+                  //   width: 6,
+                  // ),
+                  Spacer(
+                      //flex: 1
                       ),
-                      buildDot(size: 4, color: AppColors.grey16),
-                      SizedBox(
-                        height: 9,
-                      ),
-                      buildDot(size: 4, color: AppColors.grey16),
-                      SizedBox(
-                        height: 9,
-                      ),
-                      buildDot(size: 4, color: AppColors.grey16),
-                      SizedBox(
-                        height: 12.25,
-                      ),
-                      Icon(
-                        Icons.location_on_outlined,
-                        color: Color(0xffEA4335),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    width: 6,
-                  ),
-                  // Column(
-                  //   children: [
-                  //     TextField(
-                  //       controller: vm.your_location_ctrl,
-                  //       decoration: InputDecoration(
-                  //           hintText: 'Your Location',
-                  //           border: OutlineInputBorder(
-                  //               borderRadius: BorderRadius.circular(5),
-                  //               borderSide:
-                  //                   BorderSide(color: AppColors.grey16))),
-                  //     )
-                  //   ],
-                  // )
+                  Expanded(
+                    flex: 16,
+                    child: Column(
+                      children: [
+                        buildLocationTextField(
+                            vm.your_location_ctrl, 'Your Location'),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        buildLocationTextField(
+                            vm.landmark_ctrl, 'Landmark Event Center'),
+                      ],
+                    ),
+                  )
                 ],
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              Divider(),
+              Text(
+                'Results',
+                style: TextStyle(color: AppColors.grey6, fontSize: 16),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              GestureDetector(
+                onTap: (() {
+                  Navigator.of(context).pop('BA Street Shagari Estate');
+                }),
+                child: Row(
+                  children: [
+                    buildDot(
+                      size: 48,
+                      color: Color(0xffEDEEF1),
+                      child: Image.asset(
+                        'assets/images/png/Vector.png',
+                        width: 22,
+                        height: 22,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 16,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'BA Street Shagari Estate',
+                          style:
+                              TextStyle(color: AppColors.grey0, fontSize: 18),
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                        Text(
+                          'Lagos',
+                          style:
+                              TextStyle(color: AppColors.grey6, fontSize: 14),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               )
             ],
           ),
@@ -105,5 +177,16 @@ Widget buildDot({required double size, required Color color, Widget? child}) {
       borderRadius: BorderRadius.circular(100),
     ),
     child: Center(child: child),
+  );
+}
+
+Widget buildLocationTextField(TextEditingController controller, String title) {
+  return TextField(
+    controller: controller,
+    decoration: InputDecoration(
+        hintText: title,
+        border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(5),
+            borderSide: BorderSide(color: AppColors.grey16))),
   );
 }
